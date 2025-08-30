8f4f5986de5957496773e2007ef072a4b80f131c
aayushxtech <aayushxtech@gmail.com>
2025-08-30 18:55:52 +0530

    Add Google Tag Manager integration to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -188,6 +188,16 @@ export default function RootLayout({
             gtag('config', 'G-VW05WCTD9X');
           `}
         </Script>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WWPQDL2H');
+          `}
+        </Script>
         <meta name="google-site-verification" content="" />
         <meta name="msvalidate.01" content="" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -208,6 +218,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WWPQDL2H"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
